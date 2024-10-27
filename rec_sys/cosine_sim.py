60a0d8dfad790b17d9d3b73962226b55be9554c6
Fynn Orth <fynn.orth@nesto-software.de>
2024-10-27 15:36:34 +0100

    implement cosine function

--- rec_sys/cosine_sim.py
+++ rec_sys/cosine_sim.py
@@ -1,29 +1,64 @@
 import numpy as np
 import scipy.sparse as sp
+from rec_sys.cf_algorithms_to_complete import (
+    center_and_nan_to_zero,
+    cosine_sim,
+    fast_cosine_sim,
+)
 
 
-def centered_cosine_sim(vec_1, vec_2, axis=0):
-    """Compute the cosine similarity between the matrix and the vector"""
-    # Center the matrix
-    vec_1_centered = vec_1 - np.nanmean(vec_1, axis=axis)
-    vec_2_centered = vec_1 - np.nanmean(vec_2, axis=axis)
-    # Compute the cosine similarity
-    cosine_sim = np.dot(vec_1_centered, vec_2_centered) / (
-        np.linalg.norm(vec_1_centered) * np.linalg.norm(vec_2_centered)
-    )
-    return cosine_sim
+def center_and_nan_to_zero_sparse(matrix: sp.csr_matrix, axis=0):
+    """Center the matrix and replace nan values with zeros"""
+    # Compute along axis 'axis' the mean of non-nan values
+    # E.g. axis=0: mean of each column, since op is along rows (axis=0)
+    means = np.nanmean(matrix.data, axis=axis)
+    # Subtract the mean from each axis
+    matrix.data = matrix.data - means
+    matrix.data = np.nan_to_num(matrix.data)
+    return matrix
 
 
-def fast_centered_cosine_sim(vec_1, vec_2, axis=0):
-    # Convert the vectors to sparse arrays
-    vec_1_sp = sp.csr_array(vec_1)
-    vec_2_sp = sp.csr_array(vec_2)
-    # Center the sparse arrays
-    vec_1_sp -= vec_1_sp.sum() / vec_1_sp.shape[0]
-    vec_2_sp -= vec_2_sp.sum() / vec_2_sp.shape[0]
-    # Compute the cosine similarity
-    dot_product = vec_1_sp.dot(vec_2_sp)
-    norm_vec_1 = sp.linalg.norm(vec_1_sp)
-    norm_vec_2 = sp.linalg.norm(vec_2_sp)
-    cosine_sim = dot_product / (norm_vec_1 * norm_vec_2)
-    return cosine_sim
+def cosine_sim_sparse(u: sp.csr_matrix, v: sp.csr_matrix):
+    dot = u.dot(v)
+    norm_u = sp.linalg.norm(u)
+    norm_v = sp.linalg.norm(v)
+    return dot / (norm_u * norm_v)
+
+
+def fast_cosine_sim_sparse(matrix: sp.csr_matrix, vector: sp.csr_matrix, axis=0):
+    dot = matrix.dot(vector)
+    norms = sp.linalg.norm(matrix, axis=axis)
+    norm_vector = sp.linalg.norm(vector)
+    matrix.data = matrix.data / norms
+    scaled = dot / norm_vector
+    return scaled
+
+
+def centered_cosine_sim(u, v):
+    """Compute the cosine similarity between two vectors"""
+    cen_u = center_and_nan_to_zero(u)
+    cen_v = center_and_nan_to_zero(v)
+    return cosine_sim(cen_u, cen_v)
+
+
+def fast_centered_cosine_sim(matrix, vector, axis=0):
+    """Compute the cosine similarity between two vectors using sparse matrices"""
+    cen_vec = center_and_nan_to_zero(vector)
+    cen_matrix = center_and_nan_to_zero_sparse(matrix, axis=axis)
+    return fast_cosine_sim(cen_matrix, cen_vec, axis=axis)
+
+
+# Vectors for b.1)
+k = 100
+vec_x_a = np.arange(1, 101)
+vec_y_a = np.array([vec_x_a[k - 1 - i] for i in range(k)])
+
+excepted_output_a = -1
+# Vectors for b.2)
+c_values = [2, 3, 4, 5, 6]
+vec_x_b = np.array(
+    [np.nan if any((i - c) % 10 == 0 for c in c_values) else i + 1 for i in range(100)]
+)
+vec_y_b = np.array([vec_x_b[k - 1 - i] for i in range(k)])
+centered_cosine_sim(vec_x_a, vec_y_a)
+fast_centered_cosine_sim(vec_x_b, vec_y_b)
